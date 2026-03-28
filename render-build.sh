9f0d76a4b1ac74a8f6cc5767397f7853f5c2c96d
Lovaxcoding <lnantenaina78@gmail.com>
2026-03-28 10:09:05 +0300

    Remove render-build script as its functionality is no longer needed

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-apt-get update
-# On installe Tesseract ET Poppler (pour les PDF)
-apt-get install -y tesseract-ocr poppler-utils
-
-pip install -r requirements.txt
